2ff85df326e7b4ad88c432ca3c60266be28553f2
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-03-23 23:02:16 +0700

    Listing User Playlists

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,7 @@
                 <div v-if="user">
                     <router-link :to="{ name: 'CreatePlaylist' }">Create Playlist</router-link>
                     <router-link :to="{ name: 'UserPlaylists' }">My Playlists</router-link>
+                    <span>Hi there, {{ user.displayName }}</span>
                     <button @click="handleClick">Logout</button>
                 </div>
                 <div v-else>
@@ -75,4 +76,12 @@ nav .links a,
 button {
     margin-left: 16px;
     font-size: 14px;
+}
+
+span {
+    font-size: 14px;
+    display: inline-block;
+    margin-left: 16px;
+    padding-left: 16px;
+    border-left: 1px solid #eee;
 }</style>
